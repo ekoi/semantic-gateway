ed246e02e3ec63d03b9210969112bbdf3f12d483
ekoi <eko.indarto@dans.knaw.nl>
2020-10-11 08:25:49 +0200

    add configuration-list endpoint

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI, Request, Form, Response
+import os
+
+
 
 app = FastAPI()
 
 @app.get('/')
-def read_form():
+def info():
     return {"name":"semantic gateway", "version":"1.0"}
 
 @app.get('/configuration/{name}')
-def get_legacy_data(name):
+def get_configuration_data(name):
     print(name)
     try:
         content = open('data/' + name + '.xml', 'r')
         return Response(content=content.read(), media_type="application/xml")
     except:
         return "NOT FOUND"
+
+@app.get('/configuration-list')
+def get_configuration_list():
+    arr = os.listdir('./data')
+    return {"configurations": arr}
