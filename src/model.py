a2e0395ce3b4c6a9d16d53713ff7deecf7486c94
ekoi <eko.indarto@dans.knaw.nl>
2020-10-11 15:40:24 +0200

    add bootstrap

--- src/model.py
+++ src/model.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 class Vocabularies:
     "This is a Vocabularies class"
     def __init__(self, name):
-        self.root = ET.parse('./data/' + name + '.xml').getroot()
+        self.root = ET.parse('./data/' + name).getroot()
 
     def get_ontologies(self):
         ontologies = []
@@ -13,8 +13,20 @@ class Vocabularies:
         return ontologies
 
 class Ontology:
-    def __init__(self, name):
-        self.name = name.attrib['name']
+    def __init__(self, element):
+        self.element  = element
 
     def get_name(self):
-        return self.name
+        return self.element.attrib['name']
+
+    def get_type(self):
+        return self.element.find('type').text
+
+    def get_url(self):
+        base_url = self.element.find('api').text
+        uri =  self.element.find('parameters/uri')
+        if uri is not None:
+            print(uri.text)
+            return base_url +  '/' + uri.text
+        else:
+            return base_url
